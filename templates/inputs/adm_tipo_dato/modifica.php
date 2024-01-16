b028ad0086818ad5e419881028e9b8bc8226562d
MauricioHdz <mauricioisraelhernandez@gmail.com>
2024-01-16 13:20:36 -0600

    v1.16.1 Se ajustan archivos refe

--- templates/inputs/adm_tipo_dato/modifica.php
+++
@@ -1,8 +0,0 @@
-<?php /** @var gamboamartin\system\_ctl_parent $controlador  controlador en ejecucion */ ?>
-<?php use config\views; ?>
-<?php echo $controlador->inputs->descripcion; ?>
-
-<?php include (new views())->ruta_templates.'botons/submit/modifica_bd.php';?>
-
-
-
